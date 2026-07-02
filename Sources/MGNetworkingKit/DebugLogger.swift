113a7ff6ee9106866244a9cbc3660b580ed4335a
michael-g-alfred <Michoolgeorge@gmail.com>
2026-07-03 00:34:06 +0300

    Add colorized debug logging
    Introduce a Color enum with ANSI escape codes and extend DebugLogger.log(_:color:) to accept a color parameter. Messages are printed wrapped in the chosen color and reset, gated by #if DEBUG so previous behavior remains the default (.reset).

--- Sources/MGNetworkingKit/DebugLogger.swift
+++ Sources/MGNetworkingKit/DebugLogger.swift
@@ -1,10 +1,22 @@
 import Foundation
 
 enum DebugLogger {
-
-    static func log(_ message: String) {
-        #if DEBUG
-        print(message)
-        #endif
+    
+    enum Color: String {
+        case reset  = "\u{001B}[0m"
+        
+        case red    = "\u{001B}[0;31m"
+        case green  = "\u{001B}[0;32m"
+        case yellow = "\u{001B}[0;33m"
+        case blue   = "\u{001B}[0;34m"
+        case purple = "\u{001B}[0;35m"
+        case cyan   = "\u{001B}[0;36m"
+        case white  = "\u{001B}[0;37m"
+    }
+    
+    static func log(_ message: String, color: Color = .reset) {
+#if DEBUG
+        print("\(color.rawValue)\(message)\(Color.reset.rawValue)")
+#endif
     }
 }
